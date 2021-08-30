3e664ddf91a18081984a12e6618ce52a136d60cf
Jain Thomas <jainthomas87@gmail.com>
2021-08-30 21:43:16 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,10 @@
 module "s3-bucket" {
   source  = "app.terraform.io/jt-training/s3-bucket/aws"
-  version = "2.2.0"
-    bucket = "my-s3-bucket"
+  version = "2.6.0"
+  bucket = "my-s3-bucket"
   acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
 }
